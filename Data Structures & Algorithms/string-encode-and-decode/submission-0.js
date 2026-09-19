8e1bf3a32baa40ff024728aa1425f50cd8e41c85
Praise Jacob <iampraisejacob@gmail.com>
2026-09-19 22:32:24 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    
+    encode(input) {
+        let count = "";
+        let encodedString = "";
+        if (input.length > 0) {
+            count = `${input[0].length}`;
+            encodedString = `${input[0]}`;
+        }
+        for (let i = 1; i < input.length; i++) {
+            count = `${count},${input[i].length}`;
+            encodedString = `${encodedString}${input[i]}`;
+        }
+        return `${count}#${encodedString}`;
+    }
+
+    decode(input) {
+        const output = [];
+        const outputLengthMap = getLengths(input);
+        let cursor = input.indexOf("#") + 1;
+        for (const length of outputLengthMap) {
+            output.push(input.slice(cursor, cursor + length));
+            cursor += length;
+        }
+        return output;
+    }
+}
+
+function getLengths(input) {
+    const length = input.indexOf("#");
+    const lengthString = input.slice(0, length);
+    return lengthString.split(",").map(Number);
+}
